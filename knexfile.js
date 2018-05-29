4805b7f4c5f1c4e16959a9ca222182d89781c579
romo <valaroman@hotmail.com>
2018-05-29 22:15:19 +1000

    add pg

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,8 @@ var dotenv = require('dotenv');
 dotenv.load();
 
 module.exports = {
-  client: 'mysql',
-  connection: process.env.DATABASE_URL || {
+  client: 'pg',
+  connection: process.env.DATABASE_URL + '?ssl=true' || {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
